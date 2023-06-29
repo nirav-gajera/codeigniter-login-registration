120fb2b6d9e11d9af5b4d674bc9335c97f122897
Nirav Gajera <127472114+nirav-gajera@users.noreply.github.com>
2023-06-29 10:03:41 +0530

    add mail feature

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -52,8 +52,8 @@ class Register extends CI_Controller {
                 $config['protocol'] = 'smtp';
                 $config['smtp_host'] = 'sandbox.smtp.mailtrap.io';
                 $config['smtp_port'] = '2525';
-                $config['smtp_user'] = '562dc72154dc6e';
-                $config['smtp_pass'] = '0e5477e6d80214';
+                $config['smtp_user'] = '**********c6e';
+                $config['smtp_pass'] = '***********214';
                 $config['mailtype'] = 'html';
                 $config['charset'] = 'utf-8';
                 $config['newline'] = "\r\n";
@@ -77,32 +77,4 @@ class Register extends CI_Controller {
     }
     
 } 
-
-    // public function doRegister() {
-    //     $this->form_validation->set_rules('name', 'Name', 'required|min_length[3]');
-    //     $this->form_validation->set_message('is_unique', 'Email already exists.');
-    //     $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
-    //     $this->form_validation->set_rules('password', 'Password', 'required|min_length[4]');
-
-      
-    //     if ($this->form_validation->run() == FALSE) {
-    //         $this->session->set_flashdata('errors', validation_errors());
-    //         redirect(base_url() . 'register');
-    //     } else {
-    //         $name = $this->input->post('name');
-    //         $email = $this->input->post('email');
-    //         $password = sha1($this->input->post('password'));
-
-    //         $data = [
-    //             'name' => $name, 'email' => $email, 'password' => $password, 'date_time' => date('Y-m-d H:i:s')
-    //         ];
-
-    //         $insert_data = $this->register->add_user($data);
-
-    //         if ($insert_data) {
-    //             $this->session->set_flashdata('msg', 'Successfully Register, Login now!');
-    //             redirect(base_url() . 'login');
-    //         }
-    //     }
-    // }
 
